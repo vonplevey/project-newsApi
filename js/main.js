70c0aa51cab7d5b2f8d7dad35c94d91e44d33608
vonplevey <45198045+vonplevey@users.noreply.github.com>
2021-01-23 16:45:10 +0000

    newsApi - Add search button and input - Handlers for text change and submit setup

--- js/main.js
+++ js/main.js
@@ -6,16 +6,46 @@ class NewsResult extends React.Component {
   
       super(props);
     
-      this.state = { 
-  
+      this.state = {             
+        search: "",
+        newsResults: [],
       };
   
     }
 
+    handleSearchChange(event) {    
+        
+        console.log(event.target.value);
+
+        this.setState({search : event.target.value});  
+
+    }
+
+    searchNewsApi() {
+
+        console.log(1)
+
+    }
+
     render() {
 
-        return e("h1", { key: "title" }, "BT React Code Test - by Chris Plevey - 22/01/21");    
-        
+        return e("h1", { key: "title" }, "BT React Code Test - by Chris Plevey - 22/01/21"),        
+            e("div", {key: "inline-search", className: "inline-search"}, [
+            e("label",{ key: "inline-search-label", htmlFor:"article-search"}, "NewsApi Search:"),
+            e("input", {
+                key: "input",
+                type: "text", 
+                id: "article-search",
+                placeholder: "Search",          
+                value: this.state.search,
+                onChange: event => this.handleSearchChange(event)
+            }),     
+            e(
+                "button",
+                { key: "search-btn", onClick: () => this.searchNewsApi() },
+                "Search"
+            ),
+            ]);        
       }
     }  
     
